2ad37e5210c6aa49b3ea6f244dc867d1321d93f8
lelekaspb <bov-1986@yandex.ru>
2021-06-05 07:53:37 +0200

    added meaningful comments to script for ind page

--- js/individual_product.js
+++ js/individual_product.js
@@ -6,21 +6,29 @@ const sellpointVisualWidth = document.querySelector(".sellpoint_visual").offsetW
 console.log(sellpointVisualWidth);
 document.querySelector(".sellpoints").style.setProperty("--sellpoint-width", `${sellpointVisualWidth}px`);
 
+const mql = window.matchMedia("(max-width: 450px)");
+
 function countDynamicWidth() {
     const bigImageWidth = document.querySelector(".image_wrapper").offsetWidth;
     document.querySelector(".product").style.setProperty("--dynamic-width", `${bigImageWidth}px`);
     const sellpointVisualWidth = document.querySelector(".sellpoint_visual").offsetWidth;
     document.querySelector(".sellpoints").style.setProperty("--sellpoint-width", `${sellpointVisualWidth}px`);
+    if (mql.matches) {
+      console.log("This is a narrow screen — less than 450px wide.");
+      document.querySelector(".flexibility .sellpoint_text > p").textContent = "With Ullo remote you can take the light with you.";
+      document.querySelector(".colors .sellpoint_text > p").textContent = "With Ullo you get 16 million unique colors per unit of light.";
+    } else {
+      console.log("This is a wide screen — more than 450px wide.");
+      document.querySelector(".flexibility .sellpoint_text > p").textContent = "With Ullo Remote you have the ability to control your light without the use of WiFi. You can therefore also take the light with you.";
+      document.querySelector(".colors .sellpoint_text > p").textContent = "With 16 million unique colors per unit of light, you can count on near-infinite color combinations in your home.";
+    };
 }
-
-
 
 window.onresize = countDynamicWidth;
 
 
 
 // quantity plus and minus
-    
 let quantity = document.querySelector("input.quantity").value;
 document.querySelector(".btn_quantity.minus").addEventListener("click", () => {
     if (quantity > 1) {
@@ -41,6 +49,7 @@ document.querySelector(".btn_quantity.plus").addEventListener("click", () => {
 });
 
 
+// fetch data from restdb
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 console.log(id);
@@ -67,7 +76,8 @@ const url =
 
   getData();
 
-
+  
+// populate the html page
   const smallImageTemplate = document.querySelector(".small_image_template").content;
   const smallImageParent = document.querySelector(".image_wrapper");
   let i = 1;
@@ -96,17 +106,3 @@ const url =
   }
 
 
-  // change content based on the screen size
-  // stop autoplaying on mobile devices
-let mql = window.matchMedia("(max-width: 450px)");
-
-// mql.addEventListener( "change", (e) => {
-if (mql.matches) {
-  console.log("This is a narrow screen — less than 450px wide.");
-  document.querySelector(".flexibility .sellpoint_text > p").textContent = "With Ullo remote you can take the light with you.";
-  document.querySelector(".colors .sellpoint_text > p").textContent = "With Ullo you get 16 million unique colors per unit of light";
-} else {
-  /* the viewport is more than than 750 pixels wide */
-  console.log("This is a wide screen — more than 450px wide.");
-}
-// })
